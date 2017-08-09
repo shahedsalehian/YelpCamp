81fafc12fbd259af6d02bcbc11c852ce625da59d
Shahed Salehian <shahed.salehian@gmail.com>
2017-08-09 13:37:46 -0700

    mongoose installed

--- v1/app.js
+++ v1/app.js
@@ -1,21 +1,66 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-var campgrounds = [
-    {name:"Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name:"Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-    {name:"Goat Mountain", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-        {name:"Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name:"Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-    {name:"Goat Mountain", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-        {name:"Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name:"Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-    {name:"Goat Mountain", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"}
-];
+mongoose.connect("mongodb://localhost/campgrounds");
+
+var campgroundSchema = new Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// var camp = new Campground({
+//     name:"Granite Hill",
+//     image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"
+// });
+
+// camp.save(function(err, campground){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("campground saved");
+//         console.log(campground);
+//     }
+// })
+
+Campground.create({
+    name:"Goat Mountain", 
+    image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"
+}, function(err,campground){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(campground)
+    }
+});
+
+Campground.find({},function(err,campgrounds){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(campgrounds)
+    }
+});
+
+
+// var campgrounds = [
+//     {name:"Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+//     {name:"Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+//     {name:"Goat Mountain", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+//         {name:"Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+//     {name:"Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+//     {name:"Goat Mountain", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+//         {name:"Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+//     {name:"Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+//     {name:"Goat Mountain", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"}
+// ];
 
 
 //INDEX
