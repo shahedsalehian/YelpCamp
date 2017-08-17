be1ae6b1dd97fc59973b0de352ca1433babe5a30
shahedsalehian <shahed.salehian@gmail.com>
2017-08-16 17:11:35 -0700

    delete routes added

--- v1/routes/campgrounds.js
+++ v1/routes/campgrounds.js
@@ -76,6 +76,18 @@ router.put("/:id", function(req,res){
     // redirect
 });
 
+//DESTROY CAMPGROUND ROUTE
+router.delete("/:id", function(req,res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/campgrounds");
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+
 
 //middleware
 function isLoggedIn(req,res,next){
